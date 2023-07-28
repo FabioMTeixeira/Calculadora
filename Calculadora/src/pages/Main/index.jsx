41ed85212a86d5a9467d5d26367687a5c152cccd
Fabio <fabiomt.98@hotmail.com>
2023-07-28 15:47:23 -0300

    chore: template calc

--- Calculadora/src/pages/Main/index.jsx
+++ Calculadora/src/pages/Main/index.jsx
@@ -5,7 +5,36 @@ function Main() {
   const [count, setCount] = useState(0);
 
   return (
-    <div></div>
+    <div className='container'>
+      <div className='card'>
+        <div className='screen'>
+          <span>1250:20</span>
+          <h1>6250</h1>
+        </div>
+        <div className='numbers'>
+          <button>.</button>
+          <button>0</button>
+          <button>B</button>
+          <button>=</button>
+          <button>.</button>
+          <button>0</button>
+          <button>B</button>
+          <button>=</button>
+          <button>.</button>
+          <button>0</button>
+          <button>B</button>
+          <button>=</button>
+          <button>.</button>
+          <button>0</button>
+          <button>B</button>
+          <button>=</button>
+          <button>.</button>
+          <button>0</button>
+          <button>B</button>
+          <button>=</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
