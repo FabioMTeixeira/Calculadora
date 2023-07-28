69991a2b003db8e08b235ca2ca0dd718c92b3786
Fabio <fabiomt.98@hotmail.com>
2023-07-28 14:31:35 -0300

    add vite

---
+++ Calculadora/src/pages/Main/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './style.css';
+
+function Main() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div></div>
+  );
+}
+
+export default Main;
