2f39b00639f6b35faeb00014c6379f11322a30ea
Fabio <fabiomt.98@hotmail.com>
2023-07-31 01:23:35 -0300

    chore: doing calculate work without operators

--- Calculadora/src/components/Buttoncalc/index.jsx
+++ Calculadora/src/components/Buttoncalc/index.jsx
@@ -2,101 +2,121 @@ const ButtonCalc = [
     {
         id: 1,
         value: 'C',
+        operator: false,
         color: 'button-gray'
     },
     {
         id: 2,
         value: '-N',
+        operator: false,
         color: 'button-gray'
     },
     {
         id: 3,
         value: '%',
+        operator: false,
         color: 'button-gray'
     },
     {
         id: 4,
         value: '/',
+        operator: true,
         color: 'button-blue'
     },
     {
         id: 5,
         value: '7',
+        operator: false,
         color: 'normal'
     },
     {
         id: 6,
         value: '8',
+        operator: false,
         color: 'normal'
     },
     {
         id: 7,
         value: '9',
+        operator: false,
         color: 'normal'
     },
     {
         id: 8,
         value: 'x',
+        operator: true,
         color: 'button-blue'
     },
     {
         id: 9,
         value: '4',
+        operator: false,
         color: 'normal'
     },
     {
         id: 10,
         value: '5',
+        operator: false,
         color: 'normal'
     },
     {
         id: 11,
         value: '6',
+        operator: false,
         color: 'normal'
     },
     {
         id: 12,
         value: '-',
+        operator: true,
         color: 'button-blue'
     },
     {
         id: 13,
         value: '1',
+        operator: false,
         color: 'normal'
     },
     {
         id: 14,
         value: '2',
+        operator: false,
         color: 'normal'
     },
     {
         id: 15,
         value: '3',
+        operator: false,
         color: 'normal'
     },
     {
         id: 16,
         value: '+',
+        operator: true,
         color: 'button-blue'
     },
     {
         id: 17,
         value: '.',
+        operator: false,
         color: 'normal'
     },
     {
         id: 18,
         value: '0',
+        operator: false,
         color: 'normal'
     },
     {
         id: 19,
         value: 'B',
+        operator: true,
         color: 'normal'
     },
     {
         id: 20,
         value: '=',
+        operator: true,
         color: 'button-blue'
     },
 ];
--- Calculadora/src/pages/Main/index.jsx
+++ Calculadora/src/pages/Main/index.jsx
@@ -2,33 +2,51 @@ import { useState } from 'react';
 import './style.css';
 import ButtonCalc from '../../components/Buttoncalc';
 
+// fazer um state para guardar o 1 numero e outro para guardar o 2 numero, usar um if para quando ela usar um sinal, colocar na primeira ou na segunda
+
 function Main() {
   const [buttonscalc, setButtonCalc] = useState([...ButtonCalc]);
   const [touchedButton, setTouchedButton] = useState([]);
+  const [onScreen, setOnScreen] = useState('0');
+  const [operator, setOperator] = useState('');
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [discoveryNum2, setDiscoveryNum2] = useState(0);
 
   function handleShowButtons(newValue) {
     const localTouchedButton = [...touchedButton];
 
-
     const addButton = buttonscalc.find((button) => button.value === newValue);
 
-    if (addButton.value === 'C') {
-      localTouchedButton.splice(0, localTouchedButton.length);
-      setTouchedButton(localTouchedButton);
-      return;
+    if (addButton.operator && addButton.value !== '=') {
+      setNum1(localTouchedButton.join(''));
+      setOperator(addButton.value);
+      setDiscoveryNum2(localTouchedButton.length + 1);
+    } else if (addButton.operator && addButton.value === '=') {
+      setNum2(localTouchedButton.slice(discoveryNum2, localTouchedButton.length).join(''));
     }
 
     localTouchedButton.push(addButton.value);
     setTouchedButton(localTouchedButton);
 
+    if (addButton.value) {
+      setOnScreen(localTouchedButton);
+    }
+
+    if (addButton.value === 'C') {
+      localTouchedButton.splice(0, localTouchedButton.length);
+      setTouchedButton(localTouchedButton);
+      setOnScreen('0');
+      return;
+    }
   }
 
   return (
     <div className='container'>
       <div className='card'>
         <div className='screen'>
-          <span>{console.log(touchedButton)}</span>
-          <h1>6250</h1>
+          <span>{onScreen}</span>
+          <h1>{num1}{operator}={num2}</h1>
         </div>
         <div className='numbers'>
           {buttonscalc.map((buttoncalc) => (
